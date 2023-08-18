a14f6549499ed262dfd5547aca56477654f1ac55
WORK987\ezefe <ezeferrauti@gmail.com>
2023-08-18 20:39:30 -0300

    add call to service bluelytics

--- src/main/java/com/schoolofliberation/academic/Services/PriceService.java
+++ src/main/java/com/schoolofliberation/academic/Services/PriceService.java
@@ -2,11 +2,15 @@ package com.schoolofliberation.academic.Services;
 
 import com.schoolofliberation.academic.Repositories.PriceRepository;
 import com.schoolofliberation.academic.entities.Price;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -15,7 +19,24 @@ public class PriceService {
     @Autowired
     PriceRepository priceRepository;
     public ResponseEntity<Object> getAllPrice(){
+        Double dollarPrice = getDollar();
         List<Price> prices = priceRepository.findAll();
-        return new ResponseEntity<>(prices,HttpStatus.OK);
+        List<Price> actualPrices = new ArrayList<>();
+        for (Price price : prices) {
+            Double priceCareer = price.getPriceDollar() * dollarPrice ;
+            price.setPriceCareer(priceCareer);
+            actualPrices.add(price);
+        }
+        return new ResponseEntity<>(actualPrices,HttpStatus.OK);
+    }
+
+    public Double getDollar(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.bluelytics.com.ar/v2/latest";
+        HashMap<String, HashMap<String, Double>> data = restTemplate.getForObject(url, HashMap.class);
+        if(data != null){
+          return data.get("blue").get("value_sell"); 
+        }
+        return 0.0;
     }
 }
