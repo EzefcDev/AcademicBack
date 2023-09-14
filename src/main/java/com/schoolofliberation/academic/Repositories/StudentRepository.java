6df73c49815ef599db252c8e9762b15535b7422f
WORK987\ezefe <ezeferrauti@gmail.com>
2023-09-14 00:57:12 -0300

    fix repository

--- src/main/java/com/schoolofliberation/academic/Repositories/StudentRepository.java
+++ src/main/java/com/schoolofliberation/academic/Repositories/StudentRepository.java
@@ -22,5 +22,5 @@ public interface StudentRepository extends JpaRepository<Student,Long>{
     boolean existsByDni(@Param("dni") Long dni);
 
     @Query(value = "select * from students where students.id = :id and students.delete_at = '2000-01-01'", nativeQuery = true)
-    Optional<Student> findByIdWithoutDeleteAt(@Param("id") Long id)
+    Optional<Student> findByIdWithoutDeleteAt(@Param("id") Long id);
 }
